702370afdd67827e31804848f96a17c74798b329
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2023-12-12 21:59:39 +0900

    기본 연산자

---
+++ src/main/java/com/basic/basicjava/javaIntroductory/operator/Operator1.java
@@ -0,0 +1,24 @@
+package com.basic.basicjava.javaIntroductory.operator;
+
+public class Operator1 {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 2;
+
+    int sum = a + b;
+    System.out.println("sum = " + sum);
+
+    int diff = a-b;
+    System.out.println("diff = " + diff);
+
+    int multi = a * b;
+    System.out.println("multi = " + multi);
+
+    int div = a / b; //몫
+    System.out.println("div = " + div);
+
+    int mod = a % b; //나머지
+    System.out.println("mod = " + mod);
+
+  }
+}
---
+++ src/main/java/com/basic/basicjava/javaIntroductory/operator/Operator2.java
@@ -0,0 +1,22 @@
+package com.basic.basicjava.javaIntroductory.operator;
+
+public class Operator2 {
+  public static void main(String[] args) {
+    //문자열 + 문자열
+    String str1 = "Hello " + "World";
+
+    System.out.println("str1 = " + str1);
+
+    String s1 = "String1";
+    String s2 = "String2";
+    String s3 = s1 + s2;
+    System.out.println("s3 = " + s3);
+
+
+    String s4 = "4";
+    int num1 = 5;
+    String s5 = num1 + s4;
+    System.out.println("s5 = " + s5);
+
+  }
+}
