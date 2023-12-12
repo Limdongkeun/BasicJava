19e4ab0663dfd8501718c62da4429d19dfce0d6d
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2023-12-12 22:00:41 +0900

    연산자 예제

---
+++ src/main/java/com/basic/basicjava/javaIntroductory/operator/ex/OperationEx3.java
@@ -0,0 +1,12 @@
+package com.basic.basicjava.javaIntroductory.operator.ex;
+
+public class OperationEx3 {
+  public static void main(String[] args) {
+
+    int score = 80;
+
+    boolean result = score >= 80 && score <= 100;
+
+    System.out.println("result = " + result);
+  }
+}
