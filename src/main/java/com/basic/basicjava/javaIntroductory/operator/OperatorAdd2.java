f7037eac2f73d8d04cdb4e0ea95f1d60f9218e64
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2023-12-12 21:59:45 +0900

    증감 연산자

---
+++ src/main/java/com/basic/basicjava/javaIntroductory/operator/OperatorAdd2.java
@@ -0,0 +1,19 @@
+package com.basic.basicjava.javaIntroductory.operator;
+
+public class OperatorAdd2 {
+  public static void main(String[] args) {
+    //전위증감
+    int a = 1;
+    int b = 0;
+
+    b = ++a; //a의 값을 먼저 증가시키고 , 그 결과를 b에 대입
+    System.out.println("a = " + a + ", b = " + b);
+
+    //후위 증감
+    a = 1;
+    b = 0;
+
+    b = a++; //a의 값을 먼저 증가시키고, 그 결과를 b에 대입
+    System.out.println("a = " + a + ", b = " + b);
+  }
+}
