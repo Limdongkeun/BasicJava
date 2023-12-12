a67586ba31d70e4bc3d9ae2902fccaa765226be3
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2023-12-12 21:59:55 +0900

    비교 연산자

---
+++ src/main/java/com/basic/basicjava/javaIntroductory/operator/Comp1.java
@@ -0,0 +1,15 @@
+package com.basic.basicjava.javaIntroductory.operator;
+
+public class Comp1 {
+  public static void main(String[] args) {
+    int a = 2;
+    int b = 3;
+
+    System.out.println("a == b : " + (a == b));
+    System.out.println("a != b : " + (a != b));
+    System.out.println("a > b : " + (a > b));
+    System.out.println("a < b : " + (a < b));
+    System.out.println("a >= b : " + (a >= b));
+    System.out.println("a <= b : " + (a <= b));
+  }
+}
