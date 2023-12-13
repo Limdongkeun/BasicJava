bdbb4f77fad779769734baa9d4ca8d521dd13cc8
Dong Keun Lim <lim961007@naver.com>
2023-12-13 17:50:58 +0900

    DoWhile문 예제

---
+++ src/main/java/com/basic/basicjava/javaIntroductory/loop/DoWhile1.java
@@ -0,0 +1,12 @@
+package com.basic.basicjava.javaIntroductory.loop;
+
+public class DoWhile1 {
+  public static void main(String[] args) {
+    int i = 10;
+    
+    while (i < 3) {
+      System.out.println("현재 i = " + i);
+      i++;
+    }
+  }
+}
