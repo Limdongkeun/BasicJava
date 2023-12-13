a4685904fc68187e5a430509fa0cf531c61a434f
Dong Keun Lim <lim961007@naver.com>
2023-12-13 13:49:46 +0900

    if문 예제

---
+++ src/main/java/com/basic/basicjava/javaIntroductory/cond/If1.java
@@ -0,0 +1,15 @@
+package com.basic.basicjava.javaIntroductory.cond;
+
+public class If1 {
+  public static void main(String[] args) {
+    int age = 18;
+    
+    if (age >= 18) {
+      System.out.println("성인 입니다");
+    }
+    
+    if (age < 18){
+      System.out.println("미성년자 입니다");
+    }
+  }
+}
