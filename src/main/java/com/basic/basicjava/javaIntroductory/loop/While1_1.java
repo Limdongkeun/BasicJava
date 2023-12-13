021603b62aa9c96c95a9a7a7f875f855dd24044b
Dong Keun Lim <lim961007@naver.com>
2023-12-13 17:50:43 +0900

    while문 예제

---
+++ src/main/java/com/basic/basicjava/javaIntroductory/loop/While1_1.java
@@ -0,0 +1,16 @@
+package com.basic.basicjava.javaIntroductory.loop;
+
+public class While1_1 {
+  public static void main(String[] args) {
+    int count = 0;
+    
+    count += 1;
+    System.out.println("count = " + count);
+    count += 1;
+    System.out.println("count = " + count);
+    count += 1;
+    System.out.println("count = " + count);
+    
+    
+  }
+}
