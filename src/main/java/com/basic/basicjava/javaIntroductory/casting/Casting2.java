67cb99e08380a9064574d72abf143de2995e8b0e
Dong Keun Lim <lim961007@naver.com>
2023-12-14 10:31:41 +0900

    형변환 예제

--- src/main/java/com/basic/basicjava/javaIntroductory/casting/Casting2.java
+++ src/main/java/com/basic/basicjava/javaIntroductory/casting/Casting2.java
@@ -5,7 +5,7 @@ public class Casting2 {
     double doubleValue = 1.5;
     int intValue = 0;
     //intValue = doubleValue; //컴파일 오류 발생
-    intValue = (int) doubleValue; //형변환
+    intValue = (int) doubleValue; // 직접 앞에 어떤 걸로 바꿔줄지 쓰는게 명시적 형변환
     System.out.println(intValue); //출력:1
   }
 }
