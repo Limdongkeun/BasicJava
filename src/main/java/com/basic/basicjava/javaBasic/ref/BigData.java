7fd3eebadad85a3292e452dcb9201c539e382937
Dong Keun Lim <lim961007@naver.com>
2023-12-19 17:13:57 +0900

    class, method 문제풀이

---
+++ src/main/java/com/basic/basicjava/javaBasic/ref/BigData.java
@@ -0,0 +1,6 @@
+package com.basic.basicjava.javaBasic.ref;
+
+public class BigData {
+  Data data;
+  int count;
+}
---
+++ src/main/java/com/basic/basicjava/javaBasic/ref/Data.java
@@ -0,0 +1,5 @@
+package com.basic.basicjava.javaBasic.ref;
+
+public class Data {
+  int value;
+}
