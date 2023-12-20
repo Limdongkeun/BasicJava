7967ddd1a0faea7b2ec45385dab7b47831a083d4
Dong Keun Lim <lim961007@naver.com>
2023-12-20 16:43:06 +0900

    패키지 예제

---
+++ src/main/java/com/basic/basicjava/javaBasic/pack/PackageMain1.java
@@ -0,0 +1,16 @@
+package com.basic.basicjava.javaBasic.pack;
+
+//import com.basic.basicjava.javaBasic.pack.a.User;
+//import com.basic.basicjava.javaBasic.pack.a.User2;
+import com.basic.basicjava.javaBasic.pack.a.*;
+import com.basic.basicjava.javaBasic.pack.b.User;
+
+public class PackageMain1 {
+  public static void main(String[] args) {
+    Data data = new Data();
+    
+    User user = new User();
+    User2 user2 = new User2();
+    User userB = new User();
+  }
+}
