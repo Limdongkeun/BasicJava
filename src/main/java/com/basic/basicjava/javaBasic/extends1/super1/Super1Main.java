fcc7da423c40dd12546b3151581a1a721f5427d2
Dong Keun Lim <lim961007@naver.com>
2023-12-21 17:18:30 +0900

    상속 예제 6(super)

---
+++ src/main/java/com/basic/basicjava/javaBasic/extends1/super1/Super1Main.java
@@ -0,0 +1,10 @@
+package com.basic.basicjava.javaBasic.extends1.super1;
+
+public class Super1Main {
+  public static void main(String[] args) {
+    
+    Child child = new Child();
+    
+    child.call();
+  }
+}
